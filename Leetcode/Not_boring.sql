de21bad9573ee0f8d25fd0571a6659d66177459f
KartikAnand17 <63124625+KartikAnand17@users.noreply.github.com>
2023-07-19 18:42:08 +0530

    Create Not_boring.sql

---
+++ Leetcode/Not_boring.sql
@@ -0,0 +1,5 @@
+-- https://leetcode.com/problems/not-boring-movies/description/?envType=study-plan-v2&envId=top-sql-50
+
+SELECT * FROM Cinema 
+WHERE id%2!=0 AND description NOT LIKE 'boring' 
+ORDER BY rating DESC;
