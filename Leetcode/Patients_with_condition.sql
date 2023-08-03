f769075e078f83d9530d383aab043284fda19cf4
KartikAnand17 <63124625+KartikAnand17@users.noreply.github.com>
2023-08-03 19:37:12 +0530

    Create Patients_with_condition.sql

---
+++ Leetcode/Patients_with_condition.sql
@@ -0,0 +1,5 @@
+-- https://leetcode.com/problems/patients-with-a-condition/description/?envType=study-plan-v2&envId=top-sql-50
+
+SELECT *  
+FROM Patients 
+WHERE conditions LIKE "DIAB1%" OR conditions LIKE "% DIAB1%";
